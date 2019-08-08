55fef2b260b9e6e0e6e702d0b6053c0de7baec38
Arie Brainware <publicu001@protonmail.com>
2019-08-08 11:20:05 +0700

    Refactor router

--- main.go
+++ main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"net/http"
 
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	
-	//"github.com/ariebrainware/paylist-api/model"
-	"github.com/ariebrainware/paylist-api/endpoints"	
-)
 
+	ep "github.com/ariebrainware/paylist-api/endpoint"
+)
 
 type endpoint struct {
 	Method string
@@ -35,16 +32,17 @@ func main() {
 			"data":    listEndpoint,
 		})
 	})
-	// v1 := router.Group("/v1/paylist/")
-	router.GET("/paylist", endpoints.FetchAllPaylist)
-	router.GET("/paylist/:id", endpoints.FetchSinglePaylist)
-	router.POST("/paylist", endpoints.CreatePaylist)
-	router.PUT("/paylist/:id", endpoints.UpdatePaylist)
-	router.DELETE("/paylist/:id", endpoints.DeletePaylist)
-	router.GET("/users", endpoints.FetchUser)
-	router.GET("/users/:id", endpoints.FetchSingleUser)
-	router.POST("/users", endpoints.Login)
-	router.PUT("/users/:id", endpoints.UpdateUser)
-	router.DELETE("/users/:id", endpoints.DeleteUser)
+	v1 := router.Group("/v1/paylist/")
+	v1.GET("/paylist", ep.FetchAllPaylist)
+	v1.GET("/paylist/:id", ep.FetchSinglePaylist)
+	v1.POST("/paylist", ep.CreatePaylist)
+	v1.PUT("/paylist/:id", ep.UpdatePaylist)
+	v1.DELETE("/paylist/:id", ep.DeletePaylist)
+	v1.GET("/users/:id", ep.FetchSingleUser)
+	v1.GET("/users", ep.FetchUser)
+	v1.POST("/users/signin", ep.Login)
+	v1.POST("/users/signup", ep.CreateUser)
+	v1.PUT("/users/:id", ep.UpdateUser)
+	v1.DELETE("/users/:id", ep.DeleteUser)
 	router.Run(":3002")
 }
