98d0a786a2710bdb8ae374f174b046a872298ca8
Arie Brainware <publicu001@protonmail.com>
2019-09-17 11:06:50 +0700

    Code refactor

--- model/logging.go
+++ model/logging.go
@@ -3,11 +3,12 @@ package model
 import (
 	"time"
 )
+
 //Logging model for table loggings
 type Logging struct {
 	Username   string `sql:"column:username" json:"username"`
 	Token      string `sql:"column:token" json:"token"`
-	UserStatus bool   `sql:"column:userStatus"`
+	UserStatus bool   `sql:"column:user_status"`
 	CreatedAt  time.Time
 	DeletedAt  *time.Time
 }
