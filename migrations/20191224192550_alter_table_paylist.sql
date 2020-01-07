5fba755adb0739d844968ec2dc2c13bf83cf66b3
offler9 <sipitpit09@gmail.com>
2020-01-07 11:46:27 +0700

    add attribut for table paylist

--- migrations/20191224192550_alter_table_paylist.sql
+++ migrations/20191224192550_alter_table_paylist.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 ALTER table paylists
-ADD COLUMN due_date TIMESTAMP WITH TIME ZONE NULL;
+ADD COLUMN due_date DATE WITH TIME ZONE NULL;
 -- +goose Down
 ALTER table paylists
 DROP COLUMN due_date;
