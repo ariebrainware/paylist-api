0927d10edcd075fd6c28104696928b325c4243a6
Arie Brainware <publicu001@protonmail.com>
2020-01-07 14:14:38 +0700

    fixquery issue

--- migrations/20191224192550_alter_table_paylist.sql
+++ migrations/20191224192550_alter_table_paylist.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 ALTER table paylists
-ADD COLUMN due_date DATE WITH TIME ZONE NULL;
+ADD COLUMN due_date timestamp with time zone;
 -- +goose Down
 ALTER table paylists
 DROP COLUMN due_date;
