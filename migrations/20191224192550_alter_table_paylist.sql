dd1c415285ffffabf9326288aeea3282d3cd6c1f
offler9 <sipitpit09@gmail.com>
2020-06-02 18:01:16 +0700

    migration file for create and alter Paylist tables

--- migrations/20191224192550_alter_table_paylist.sql
+++ migrations/20191224192550_alter_table_paylist.sql
@@ -1,8 +1,8 @@
--- +goose Up
--- SQL in this section is executed when the migration is applied.
+-- -- +goose Up
+-- -- SQL in this section is executed when the migration is applied.
 ALTER table paylists
-ADD COLUMN due_date timestamp with time zone;
--- +goose Down
+ADD COLUMN due_date TIMESTAMP WITH TIME ZONE NULL;
+-- -- +goose Down
 ALTER table paylists
 DROP COLUMN due_date;
--- SQL in this section is executed when the migration is rolled back.
+-- -- SQL in this section is executed when the migration is rolled back.
