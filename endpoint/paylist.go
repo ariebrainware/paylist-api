fe98d7c42b687a6bed91afc1adc479ffc83df356
Arie Brainware <publicu001@protonmail.com>
2024-09-18 18:51:34 +0700

    Refactor paylist endpoint: Update FetchAllPaylist, FetchSinglePaylist, CreateUserPaylist, UpdateUserPaylist, UpdateUserPaylistStatus, DeleteUserPaylist, and FilterPaylist functions

--- endpoint/paylist.go
+++ endpoint/paylist.go
@@ -7,7 +7,6 @@ import (
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 
 	"github.com/ariebrainware/paylist-api/config"
 	"github.com/ariebrainware/paylist-api/model"
@@ -20,15 +19,20 @@ type User struct {
 	jwt.StandardClaims
 }
 
+// FetchAllPaylist handles the request to fetch all paylists for a user.
+// It retrieves the user's token from the request context, parses it to get the username,
+// and then queries the database for paylists associated with that username.
+// If the token is invalid or there are no paylists found, appropriate error responses are returned.
+// On success, it returns the list of paylists.
+//
+// @param c *gin.Context - The Gin context for the request.
+//
+// @response 200 - Successfully fetched all paylists.
+// @response 400 - Failed to parse the token or no paylists found.
 func FetchAllPaylist(c *gin.Context) {
 	var paylist []model.Paylist
-	tk := User{}
-	tokenString := c.Request.Header.Get("Authorization")
-	token, err := jwt.ParseWithClaims(tokenString, &tk, func(token *jwt.Token) (interface{}, error) {
-		return []byte(fmt.Sprint(config.Conf.JWTSignature)), nil
-	})
-	if err != nil || token == nil {
-		fmt.Println(err, token)
+	tk, err := parseToken(c)
+	if err != nil {
 		util.CallServerError(c, util.APIErrorParams{
 			Msg: "fail to parse the token, make sure token and signature is valid",
 			Err: err,
@@ -50,6 +54,24 @@ func FetchAllPaylist(c *gin.Context) {
 	})
 }
 
+// FetchSinglePaylist handles the request to fetch a single paylist by its ID.
+// It expects an Authorization token in the request header and validates it.
+// If the token is valid, it retrieves the paylist associated with the given ID
+// and the username extracted from the token. If the paylist is found, it returns
+// the paylist data with a success message. If any error occurs, it returns an
+// appropriate error message.
+//
+// @Summary Fetch a single paylist
+// @Description Fetch a single paylist by its ID and the username from the token
+// @Tags paylist
+// @Accept json
+// @Produce json
+// @Param id path string true "Paylist ID"
+// @Param Authorization header string true "Bearer token"
+// @Success 200 {object} model.Paylist "success fetch single paylist"
+// @Failure 400 {object} util.APIErrorParams "fail to parse the token, make sure token is valid"
+// @Failure 404 {object} util.APIErrorParams "no paylist found!"
+// @Router /paylist/{id} [get]
 func FetchSinglePaylist(c *gin.Context) {
 	var paylist model.Paylist
 	paylistID := c.Param("id")
@@ -81,6 +103,23 @@ func FetchSinglePaylist(c *gin.Context) {
 	})
 }
 
+// CreateUserPaylist handles the creation of a new paylist item for a user.
+// It performs the following steps:
+// 1. Parses the JWT token from the Authorization header to extract the username.
+// 2. Decreases the user's balance by the amount specified in the request.
+// 3. Creates a new paylist item with the provided details (name, amount, due date).
+// 4. Saves the new paylist item to the database.
+// 5. Returns a success response with the ID of the created paylist item.
+//
+// Parameters:
+// - c: *gin.Context - The Gin context, which contains the request and response objects.
+//
+// The function expects the following form data in the request:
+// - name: string - The name of the paylist item.
+// - amount: string - The amount to be deducted from the user's balance.
+// - due_date: string - The due date of the paylist item in the format "YYYY-MM-DD".
+//
+// The function returns a JSON response with the status of the operation.
 func CreateUserPaylist(c *gin.Context) {
 	users := model.User{}
 	tk := User{}
@@ -136,48 +175,57 @@ func CreateUserPaylist(c *gin.Context) {
 
 }
 
+// UpdateUserPaylist updates the paylist of a user based on the provided context.
+// It performs the following steps:
+// 1. Parses the token from the context to get the username.
+// 2. Retrieves the user and paylist based on the username and paylist ID.
+// 3. Validates if the user is authorized to update the paylist.
+// 4. Updates the paylist with the new data provided in the request.
+// 5. Updates the user's balance based on the changes in the paylist amount.
+// 6. Returns appropriate error messages if any step fails, or a success message if the update is successful.
+//
+// Parameters:
+// - c: *gin.Context - The context of the request, containing parameters and form data.
+//
+// Possible Errors:
+// - If the token is invalid or cannot be parsed.
+// - If the user or paylist is not found.
+// - If the user is not authorized to update the paylist.
+// - If there is an error updating the paylist or user balance.
+//
+// Success Response:
+// - Returns a success message indicating the paylist was successfully updated.
 func UpdateUserPaylist(c *gin.Context) {
-	paylist := model.Paylist{}
-	user := model.User{}
-	tk := User{}
-
-	// Parse the token payload
-	tokenString := c.GetHeader("Authorization")
-	token, err := jwt.ParseWithClaims(tokenString, &tk, func(token *jwt.Token) (interface{}, error) {
-		return []byte(fmt.Sprint(config.Conf.JWTSignature)), nil
-	})
-	if err != nil || token == nil {
-		fmt.Println(err, token)
+	tk, err := parseToken(c)
+	if err != nil {
 		util.CallUserError(c, util.APIErrorParams{
 			Msg: "fail to parse the token, make sure the token is valid",
 			Err: err,
 		})
+		return
 	}
 
-	// Check User balance
 	username := tk.Username
 	id, _ := strconv.Atoi(c.Param("id"))
-	if err = config.DB.Model(&user).Select("balance").Where("username = ?", username).Find(&user).Error; err != nil {
+
+	user, err := findUserByUsername(username)
+	if err != nil {
 		util.CallErrorNotFound(c, util.APIErrorParams{
 			Msg: "user not found",
 			Err: err,
 		})
 		return
 	}
-	if err = config.DB.Model(&paylist).Where("ID = ? AND username = ?", id, username).First(&paylist).Error; err != nil || err == gorm.ErrRecordNotFound {
+
+	paylist, err := findPaylistByIDAndUsername(id, username)
+	if err != nil {
 		util.CallErrorNotFound(c, util.APIErrorParams{
 			Msg: "no paylist found",
-			Err: nil,
+			Err: err,
 		})
 		return
 	}
-	firstAmount := paylist.Amount
-	amount, _ := strconv.Atoi(c.PostForm("amount"))
-	updatedPaylist := model.Paylist{
-		Name:    c.PostForm("name"),
-		Amount:  amount,
-		DueDate: c.PostForm("due_date"),
-	}
+
 	if tk.Username != paylist.Username {
 		util.CallServerError(c, util.APIErrorParams{
 			Msg: "not authorized",
@@ -185,9 +233,16 @@ func UpdateUserPaylist(c *gin.Context) {
 		})
 		return
 	}
-	// Update paylist
-	if err = config.DB.Model(&paylist).Where("username = ?", username).Update(&updatedPaylist).Error; err != nil {
-		fmt.Println(err)
+
+	firstAmount := paylist.Amount
+	amount, _ := strconv.Atoi(c.PostForm("amount"))
+	updatedPaylist := model.Paylist{
+		Name:    c.PostForm("name"),
+		Amount:  amount,
+		DueDate: c.PostForm("due_date"),
+	}
+
+	if err := updatePaylist(username, updatedPaylist); err != nil {
 		util.CallServerError(c, util.APIErrorParams{
 			Msg: "something error when try to update paylist",
 			Err: err,
@@ -195,51 +250,97 @@ func UpdateUserPaylist(c *gin.Context) {
 		return
 	}
 
-	// Update user balance
-	err = config.DB.Model(&user).Where("username = ?", username).Update("balance", (firstAmount-amount)+user.Balance).Error
-	if err != nil {
-		fmt.Println(err)
+	if err := updateUserBalance(username, firstAmount, amount, user.Balance); err != nil {
 		util.CallServerError(c, util.APIErrorParams{
 			Msg: "something error when try to update user balance",
 			Err: err,
 		})
 		return
 	}
+
 	util.CallSuccessOK(c, util.APISuccessParams{
 		Msg:  "paylist successfully updated!",
 		Data: paylist,
 	})
 }
 
-func UpdateUserPaylistStatus(c *gin.Context) {
-	paylist := model.Paylist{}
-	user := model.User{}
+func parseToken(c *gin.Context) (*User, error) {
 	tk := User{}
-
-	// Parse the token payload and validate the username is own the paylist
 	tokenString := c.GetHeader("Authorization")
-	token, err := jwt.ParseWithClaims(tokenString, &tk, func(token *jwt.Token) (interface{}, error) {
+	_, err := jwt.ParseWithClaims(tokenString, &tk, func(token *jwt.Token) (interface{}, error) {
 		return []byte(fmt.Sprint(config.Conf.JWTSignature)), nil
 	})
-	if err != nil || token == nil {
-		fmt.Println(err, token)
+	if err != nil {
+		return nil, err
+	}
+	return &tk, nil
+}
+
+func findUserByUsername(username string) (*model.User, error) {
+	user := model.User{}
+	err := config.DB.Model(&user).Select("balance").Where("username = ?", username).Find(&user).Error
+	return &user, err
+}
+
+func findPaylistByIDAndUsername(id int, username string) (*model.Paylist, error) {
+	paylist := model.Paylist{}
+	err := config.DB.Model(&paylist).Where("ID = ? AND username = ?", id, username).First(&paylist).Error
+	return &paylist, err
+}
+
+func updatePaylist(username string, updatedPaylist model.Paylist) error {
+	return config.DB.Model(&model.Paylist{}).Where("username = ?", username).Update(&updatedPaylist).Error
+}
+
+func updateUserBalance(username string, firstAmount, amount, balance int) error {
+	return config.DB.Model(&model.User{}).Where("username = ?", username).Update("balance", (firstAmount-amount)+balance).Error
+}
+
+// UpdateUserPaylistStatus updates the status of a user's paylist.
+//
+// This function performs the following steps:
+// 1. Parses the token from the request context.
+// 2. Retrieves the username from the token.
+// 3. Converts the "id" parameter from the request to an integer.
+// 4. Finds the paylist by ID and username.
+// 5. Finds the user by username.
+// 6. Updates the paylist status based on the user's balance.
+// 7. Returns a success response if the update is successful.
+//
+// If any step fails, an appropriate error response is returned.
+//
+// Parameters:
+// - c: The Gin context containing the request and response objects.
+//
+// Responses:
+// - 200 OK: If the paylist status is successfully updated.
+// - 400 Bad Request: If the token parsing fails.
+// - 404 Not Found: If the paylist or user is not found.
+// - 500 Internal Server Error: If updating the paylist status fails.
+func UpdateUserPaylistStatus(c *gin.Context) {
+	tk, err := parseToken(c)
+	if err != nil {
 		util.CallUserError(c, util.APIErrorParams{
 			Msg: "fail to parse the token, make sure the token is valid",
 			Err: err,
 		})
+		return
 	}
-	username := tk.Username
 
-	// Check User balance
+	username := tk.Username
 	id, _ := strconv.Atoi(c.Param("id"))
-	if err := config.DB.Model(&paylist).Where("ID = ? AND username = ?", id, username).First(&paylist).Error; err != nil || err == gorm.ErrRecordNotFound {
+
+	paylist, err := findPaylistByIDAndUsername(id, username)
+	if err != nil {
 		util.CallErrorNotFound(c, util.APIErrorParams{
 			Msg: "no paylist found",
-			Err: nil,
+			Err: err,
 		})
 		return
 	}
-	if err := config.DB.Model(&user).Select("balance").Where("username = ?", username).First(&user).Error; err != nil {
+
+	user, err := findUserByUsername(username)
+	if err != nil {
 		util.CallErrorNotFound(c, util.APIErrorParams{
 			Msg: "user not found",
 			Err: err,
@@ -247,26 +348,31 @@ func UpdateUserPaylistStatus(c *gin.Context) {
 		return
 	}
 
-	// Update the paylist status
-	if user.Balance >= 0 && paylist.Completed == false {
-		paylist.Completed = true
-		config.DB.Model(&paylist).Where("ID = ? and username = ?", id, username).Update(&paylist)
-	} else if user.Balance < 0 && paylist.Completed == false {
-		paylist.Completed = false
-		config.DB.Model(&paylist).Where("ID = ? and username = ?", id, username).Update(&paylist)
+	if err := updatePaylistStatus(paylist, user.Balance); err != nil {
+		util.CallServerError(c, util.APIErrorParams{
+			Msg: "fail to update paylist status",
+			Err: err,
+		})
+		return
 	}
+
 	util.CallSuccessOK(c, util.APISuccessParams{
 		Msg:  "successfully update user paylist",
 		Data: paylist.Completed,
 	})
 }
 
+func updatePaylistStatus(paylist *model.Paylist, balance int) error {
+	if balance >= 0 && !paylist.Completed {
+		paylist.Completed = true
+	} else if balance < 0 && !paylist.Completed {
+		paylist.Completed = false
+	}
+	return config.DB.Model(&paylist).Update(paylist).Error
+}
+
 func DeleteUserPaylist(c *gin.Context) {
 	paylistID, _ := strconv.Atoi(c.Param("id"))
-	paylist := &model.Paylist{}
-	user := &model.User{}
-	tk := User{}
-
 	if paylistID == 0 {
 		util.CallUserError(c, util.APIErrorParams{
 			Msg: "please specify paylist id",
@@ -274,76 +380,94 @@ func DeleteUserPaylist(c *gin.Context) {
 		})
 		return
 	}
-	// Parse the token payload and validate the username is own the paylist
-	tokenString := c.GetHeader("Authorization")
-	token, err := jwt.ParseWithClaims(tokenString, &tk, func(token *jwt.Token) (interface{}, error) {
-		return []byte(fmt.Sprint(config.Conf.JWTSignature)), nil
-	})
-	if err != nil || token == nil {
-		fmt.Println(err, token)
+
+	tk, err := parseToken(c)
+	if err != nil {
 		util.CallUserError(c, util.APIErrorParams{
 			Msg: "fail to parse the token, make sure the token is valid",
 			Err: err,
 		})
+		return
 	}
-	username := tk.Username
-	if err = config.DB.Where("ID = ?", paylistID).Find(&paylist).Error; err != nil {
+
+	paylist, err := findPaylistByID(paylistID)
+	if err != nil {
 		util.CallErrorNotFound(c, util.APIErrorParams{
 			Msg: "no paylist found!",
 			Err: err,
 		})
-		c.Abort()
 		return
 	}
 
-	config.DB.Model(&paylist).Where("username = ?", username).First(&paylist)
 	if tk.Username != paylist.Username {
 		util.CallServerError(c, util.APIErrorParams{
 			Msg: "user not authorized",
 			Err: nil,
 		})
-		c.Abort()
 		return
 	}
 
-	if err = config.DB.Model(&paylist).Select("amount, completed").Where("ID = ? and username = ?", paylistID, username).First(&paylist).Error; err != nil {
-		util.CallErrorNotFound(c, util.APIErrorParams{
-			Msg: "can't select amount",
-			Err: err,
-		})
-		return
-	}
-
-	if err := config.DB.Model(&user).Select("balance").Where("username = ?", username).First(&user).Error; err != nil {
+	user, err := findUserByUsername(tk.Username)
+	if err != nil {
 		util.CallErrorNotFound(c, util.APIErrorParams{
 			Msg: "can't select balance",
 			Err: err,
 		})
 		return
 	}
-	if paylist.Completed == false {
-		b := paylist.Amount + user.Balance
-		if err = config.DB.Table("users").Where("username = ?", username).Update("balance", b).Error; err != nil {
-			fmt.Println(&user.Balance)
-			util.CallServerError(c, util.APIErrorParams{
-				Msg: "fail to update the user balance",
-				Err: err,
-			})
-			return
-		}
+
+	if err := updateUserBalanceOnDelete(paylist, user); err != nil {
+		util.CallServerError(c, util.APIErrorParams{
+			Msg: "fail to update the user balance",
+			Err: err,
+		})
+		return
 	}
-	if err := config.DB.Model(&paylist).Where("ID = ? and username = ?", paylistID, username).Delete(&paylist).Error; err != nil {
+
+	if err := deletePaylist(paylistID, tk.Username); err != nil {
 		util.CallServerError(c, util.APIErrorParams{
 			Msg: "paylist fail to delete",
 			Err: err,
 		})
+		return
 	}
+
 	util.CallSuccessOK(c, util.APISuccessParams{
 		Msg:  "paylist successfully deleted!",
 		Data: nil,
 	})
 }
 
+func findPaylistByID(paylistID int) (*model.Paylist, error) {
+	paylist := &model.Paylist{}
+	err := config.DB.Where("ID = ?", paylistID).First(&paylist).Error
+	return paylist, err
+}
+
+func updateUserBalanceOnDelete(paylist *model.Paylist, user *model.User) error {
+	if !paylist.Completed {
+		newBalance := paylist.Amount + user.Balance
+		return config.DB.Table("users").Where("username = ?", user.Username).Update("balance", newBalance).Error
+	}
+	return nil
+}
+
+func deletePaylist(paylistID int, username string) error {
+	return config.DB.Where("ID = ? and username = ?", paylistID, username).Delete(&model.Paylist{}).Error
+}
+
+// FilterPaylist filters the paylist based on the provided "created_at" parameter and the username extracted from the JWT token.
+// It expects the "Authorization" header to contain a valid JWT token.
+//
+// @param c *gin.Context - The Gin context which provides request and response handling.
+//
+// The function performs the following steps:
+// 1. Retrieves the "created_at" parameter from the request context.
+// 2. If the "created_at" parameter is not provided, it returns an error response indicating the missing parameter.
+// 3. Extracts the JWT token from the "Authorization" header and parses it to extract the username.
+// 4. Queries the database to filter the paylist records based on the username and the month of the "created_at" parameter.
+// 5. If the query is successful, it returns the filtered paylist in the response.
+// 6. If any error occurs during the process, it returns an appropriate error response.
 func FilterPaylist(c *gin.Context) {
 	paylist := model.Paylist{}
 	//param,_ := strconv.ParseInt(c.Param("month"),10,64)
